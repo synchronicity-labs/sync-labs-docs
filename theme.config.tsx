ee000d09783d40ccd4f9ec3f7d4f9f78fa7215d6
Noah Milberger <n.milberger@gmail.com>
2023-10-25 15:12:40 -0700

    Added favicon. Created billing page and added info.

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 import logo from "@/assets/sync_stack_white.png";
 import SlackLogo from "@/assets/SlackLogo";
 
+import { useRouter } from "next/router";
+import { useConfig } from "nextra-theme-docs";
+
 export default {
   logo: <Image src={logo} alt="Sync Labs logo" width="120" />,
   project: {
@@ -16,5 +19,39 @@ export default {
     "https://github.com/synchronicity-labs/sync-labs-docs/blob/main",
   footer: {
     text: <Image src={logo} alt="Sync Labs logo" width="120" />,
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      "https://docs.tryleap.ai" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+    const googleAnalyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+
+    return (
+      <>
+        <link rel="icon" href="https://app.synclabs.so/favicon.ico" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || "Sync Labs"} />
+        <meta
+          property="og:description"
+          content={
+            frontMatter.description ||
+            "sync any video to any audio in any language — no training required."
+          }
+        />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+        ></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${googleAnalyticsId}');`}
+        </script>
+      </>
+    );
   },
 };
